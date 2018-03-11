148d64a58f35954d86a60f4be3da69d1ff781fb6
Michael Kruszczynski <michaelkruszczynski@yahoo.co.uk>
2018-03-11 17:00:37 +0000

    Add department HTML

--- src/main/java/dis/Department.java
+++ src/main/java/dis/Department.java
@@ -1,12 +1,15 @@
 package dis;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Department {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String departmentName;
 
--- src/main/java/dis/DepartmentController.java
+++ src/main/java/dis/DepartmentController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,6 +27,12 @@ public class DepartmentController {
 	public String createForm(Model model) {
 		model.addAttribute("department", new Department());
 		return "departmentAdd";
+	}
+
+	@PostMapping(path = "/add")
+	public String createSubmit(@ModelAttribute Department department) {
+		departmentRepository.save(department);
+		return "departmentAddResult";
 	}
 
 	@GetMapping(path = "/all")
