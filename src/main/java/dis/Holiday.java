8e04263b49e65461f392e676800711c5e0c4241d
Michael Kruszczynski <michaelkruszczynski@yahoo.co.uk>
2018-08-07 11:01:35 +0100

    CRUD for holiday added, but data data appear......

--- src/main/java/dis/Holiday.java
+++ src/main/java/dis/Holiday.java
@@ -1,19 +1,22 @@
 package dis;
 
-import java.sql.Timestamp;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Holiday {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	private Timestamp day;
+	@Temporal(TemporalType.DATE)
+	private Date day;
 
 	public long getId() {
 		return id;
@@ -23,11 +26,11 @@ public class Holiday {
 		this.id = id;
 	}
 
-	public Timestamp getDay() {
+	public Date getDay() {
 		return day;
 	}
 
-	public void setDay(Timestamp day) {
+	public void setDay(Date day) {
 		this.day = day;
 	}
 
