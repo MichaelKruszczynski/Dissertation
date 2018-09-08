d7a47afe8f4d1b8c11f55a6aca4c474230002774
Michael Kruszczynski <michaelkruszczynski@yahoo.co.uk>
2018-09-08 15:58:26 +0100

    Report class added

---
+++ src/main/java/dis/Report.java
@@ -0,0 +1,66 @@
+package dis;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "name" }) })
+public class Report {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@NotNull
+	@Size(min = 1, message = "Please input raport name")
+	private String name;
+
+	@NotNull
+	@Size(min = 1, message = "Please input a query ")
+	private String query;
+	@NotNull
+	@Size(min = 1, message = "Please input a column names ")
+	private String columnNames;
+
+	@NotNull
+	private String accessLevel;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	public String getColumnNames() {
+		return columnNames;
+	}
+
+	public void setColumnNames(String columnNames) {
+		this.columnNames = columnNames;
+	}
+
+	public String getAccessLevel() {
+		return accessLevel;
+	}
+
+	public void setAccessLevel(String accessLevel) {
+		this.accessLevel = accessLevel;
+	}
+
+}
