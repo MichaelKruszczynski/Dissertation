019cfaa240757ec4da9e890b4124ca99596760cd
Michael Kruszczynski <michaelkruszczynski@yahoo.co.uk>
2018-09-29 14:48:17 +0100

    role--admin/user added

--- src/main/java/dis/Employee.java
+++ src/main/java/dis/Employee.java
@@ -31,9 +31,6 @@ public class Employee {
 
 	private EmployeeType type;
 
-	@NotNull
-	private AccessLevel accessLevel;
-
 	@Email
 	@Size(min = 1, message = "Please input email address")
 	private String email;
@@ -192,14 +189,6 @@ public class Employee {
 
 	public void setId(long id) {
 		this.id = id;
-	}
-
-	public AccessLevel getAccessLevel() {
-		return accessLevel;
-	}
-
-	public void setAccessLevel(AccessLevel accessLevel) {
-		this.accessLevel = accessLevel;
 	}
 
 }
