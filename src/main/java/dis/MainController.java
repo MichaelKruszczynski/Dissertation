cc8053ca764bdee715ebd9f956051c005e7a0b20
Michael Kruszczynski <michaelkruszczynski@yahoo.co.uk>
2018-10-05 19:10:40 +0100

    Add new Index (will fill it later)

--- src/main/java/dis/MainController.java
+++ src/main/java/dis/MainController.java
@@ -1,13 +1,39 @@
 package dis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @EnableAutoConfiguration
 // This means that this class is a Controller
 // This means URL's start with /demo (after Application path)
-	@RequestMapping(path = "/")
+@RequestMapping(path = "/")
 public class MainController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@RequestMapping
+	public String main(Model model) {
+		if (getCurrentUser().hasRole(ProjectNames.ROLE_ADMIN)) {
+			model.addAttribute("userLevel", ProjectNames.ROLE_ADMIN);
+		} else if (getCurrentUser().hasRole(ProjectNames.ROLE_MANAGER)) {
+			model.addAttribute("userLevel", ProjectNames.ROLE_MANAGER);
+		} else {
+			model.addAttribute("userLevel", ProjectNames.ROLE_USER);
+		}
+		return "index";
+	}
+
+	private Employee getCurrentUser() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		MyUserPrincipal principal = (MyUserPrincipal) authentication.getPrincipal();
+		Employee employee = principal.getEmployee();
+		return employeeRepository.findOne(employee.getId());
+	}
 }
